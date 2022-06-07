135c446d714b4fd8f71e736509d9bd8b5456943f
Meme <meme_es@hotmail.com>
2022-06-07 07:47:17 -0600

    Add layout to draw the calculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css';
+import logo from './images/logo.png';
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <div className='container-logo'>
+        <img
+          className='logo'
+          src={logo}
+          alt='Image logo'
+        />
+      </div>
+      <div className='container-calc'>
+        <div className='row'>
+        </div>
+        <div className='row'>
+        </div>
+        <div className='row'>
+        </div>
+        <div className='row'>
+        </div>
+        <div className='row'>
+        </div>
+      </div>
     </div>
   );
 }
